ef746de9721cda2d107c28079c6561c2ad0f9eff
arianaggm <144489620+arianaggm@users.noreply.github.com>
2023-10-12 12:42:30 -0600

    Create Characters.h

---
+++ Characters.h
@@ -0,0 +1,29 @@
+#ifndef CHARS_H
+#define CHARS_H
+#include <xc.h>
+
+#define DROP1 0
+#define DROP2 1
+
+char adrop[]={
+    0b00100,
+    0b01100,
+    0b01110,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,   
+};
+
+char bdrop[]={
+    0b00100,
+    0b00110,
+    0b01110,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,   
+};
+#endif /* __cplusplus */
